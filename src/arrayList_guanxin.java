9b56a49658765ab32ffbb6a71fd01bab8b31c881
Guanxin <liguanxin078@gmail.com>
2018-01-11 11:41:41 -0800

    Jan11

--- src/arrayList_guanxin.java
+++ src/arrayList_guanxin.java
@@ -1,4 +1,33 @@
 public class arrayList_guanxin
 {
 
+    private int sizeList;
+    private int size;
+
+
+    /**
+     * Constructor -- for sizeList
+     */
+    public arrayList_guanxin()
+    {
+
+    }
+
+    /**
+     * Return the size of the arrayList
+     * @return the number of items in the array
+     */
+    public int sizeList()
+    {
+        return this.sizeList;
+    }
+
+    /**
+     * @return the size of the array
+     */
+    public int size()
+    {
+        return this.size;
+    }
+
 }
